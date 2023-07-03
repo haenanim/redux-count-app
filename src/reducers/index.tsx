e842b093e131058dd7b5560c94468b71b1ec6166
haenanim121@gmail.com <haenanim121@gmail.com>
2023-07-03 19:24:03 +0900

    feat : axios 패키지 설치, posts 생성

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import todoList from './todoList';
+import posts from './posts';
 
 const rootReducer = combineReducers({
   todoList,
   counter,
+  posts,
 });
 export default rootReducer;
 
